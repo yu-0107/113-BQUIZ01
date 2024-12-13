19c46d727d948e9ddbdbc3cb8798192e0a851ffe
onion <a0966550619@gmail.com>
2024-12-13 14:20:36 +0800

    完成前後台的網站標題,進站總人數及頁尾版權顯示,以及首頁的跑馬燈顯示

--- admin.php
+++ admin.php
@@ -76,7 +76,7 @@
                 </div>
                 <div class="dbor" style="margin:3px; width:95%; height:20%; line-height:100px;">
                     <span class="t">進站總人數 :
-                        1 </span>
+                        <?=$Total->find(1)['total'];?></span>
                 </div>
             </div>
             <?php
@@ -94,7 +94,7 @@
         <div style="clear:both;"></div>
         <div
             style="width:1024px; left:0px; position:relative; background:#FC3; margin-top:4px; height:123px; display:block;">
-            <span class="t" style="line-height:123px;"></span>
+            <span class="t" style="line-height:123px;"><?=$Bottom->find(1)['bottom'];?></span>
         </div>
     </div>
 
--- front/main.php
+++ front/main.php
@@ -1,6 +1,14 @@
 <div class="di"
     style="height:540px; border:#999 1px solid; width:53.2%; margin:2px 0px 0px 0px; float:left; position:relative; left:20px;">
     <marquee scrolldelay="120" direction="left" style="position:absolute; width:100%; height:40px;">
+        <?php
+    $ads=$Ad->all(['sh'=>1]);
+    foreach($ads as $ad){
+        echo $ad['text'];
+        // echo "&nbsp;&nbsp;&nbsp;&nbsp;";
+        echo str_repeat("&nbsp;",4);
+    }
+?>
     </marquee>
     <div style="height:32px; display:block;"></div>
     <!--正中央-->
