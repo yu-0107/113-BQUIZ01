a567f4c8495591d809499bc14cc379f7d16e7704
onion <a0966550619@gmail.com>
2024-12-17 15:55:13 +0800

    完成校園映像顯示和主選單顯示

--- index.php
+++ index.php
@@ -35,6 +35,17 @@
                 <div id="menuput" class="dbor">
                     <!--主選單放此-->
                     <span class="t botli">主選單區</span>
+                    <?php
+                        $mains=$Menu->all(['sh'=>1,'main_id'=>0]);
+                        foreach($mains as $main){
+                            echo "<div class='mainmu cent'>";
+                            echo "<a href='{$main['href']}'>";
+                            echo $main['text'];
+                            echo "</a>";
+                            echo "</div>";
+                        }
+
+                    ?>
                 </div>
                 <div class="dbor" style="margin:3px; width:95%; height:20%; line-height:100px;">
                     <span class="t">進站總人數 :
@@ -59,16 +70,36 @@
                     onclick="lo(&#39;?do=admin&#39;)">管理登入</button>
                 <div style="width:89%; height:480px;" class="dbor">
                     <span class="t botli">校園映象區</span>
+                    <div class="cent" id="up" onclick="pp(1)">
+                        <img src="./icon/up.jpg" alt="" srcset="">
+                    </div>
+                    <div class="cent">
+                        <?php
+                    $imgs=$Image->all(['sh'=>1]);
+                    foreach($imgs as $idx => $img){
+                        echo "<div class='im' id='ssaa{$idx}'>";
+                        echo "<img src='./upload/{$img['img']}' style='width:150px;height:103px;border:3px solid orange'>"; 
+                        echo "</div>";  
+                    }
+                    ?>
+                    </div>
+                    <div class="cent" id="dn" onclick="pp(2)">
+                        <img src="./icon/dn.jpg" alt="" srcset="">
+                    </div>
+
+
+
+
                     <script>
                     var nowpage = 0,
-                        num = 0;
+                        num = <?=$Image->count(['sh'=>1]);?>;
 
                     function pp(x) {
                         var s, t;
                         if (x == 1 && nowpage - 1 >= 0) {
                             nowpage--;
                         }
-                        if (x == 2 && (nowpage + 1) * 3 <= num * 1 + 3) {
+                        if (x == 2 && (nowpage + 1) <= num * 1 - 3) {
                             nowpage++;
                         }
                         $(".im").hide()
