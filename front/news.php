b114d0be87e11b4732cd6db7e9acb71055df2be2
onion <a0966550619@gmail.com>
2024-12-17 10:12:28 +0800

    完成最新消息顯示及更多消息顯示頁和分頁功能

--- front/news.php
+++ front/news.php
@@ -4,10 +4,53 @@
     </marquee>
     <div style="height:32px; display:block;"></div>
     <!--正中央-->
-    <div style="text-align:center;">
-        <a class="bl" style="font-size:30px;" href="?do=meg&p=0">&lt;&nbsp;</a>
-        <a class="bl" style="font-size:30px;" href="?do=meg&p=0">&nbsp;&gt;</a>
-    </div>
+    <h3>更多最新消息資料區</h3>
+    <hr>
+    <ul class="ssaa" style="list-style-type:decimal;">
+        <?php   
+            $div=5;
+                $total=$News->count();
+                $pages=ceil($total/$div);
+                $now=$_GET['p']??1;
+                $start=($now-1)*$div;
+                $rows=$News->all(" limit $start,$div");
+                echo "<ol start='".($start+1)."'>";
+
+            foreach($rows as $row){
+                echo "<li>";
+                echo mb_substr($row['text'],0,20);
+                        echo "<span class='all' style='display:none'>";
+                        echo $row['text'];
+                        echo "</span>";
+                    echo "</li>";
+            }
+
+        ?>
+
+        </ol>
+        <div class="cent">
+            <?php
+
+                if(($now-1)>0){
+                    $prev=$now-1;
+                    echo "<a href='?do=$do&p=$prev'> < </a>";
+                }
+
+                
+                
+                for($i=1;$i<=$pages;$i++){
+                    echo "<a href='?do=$do&p=$i'> ";
+                    echo $i;
+                    echo " </a>";
+                }
+
+                if(($now+1)<=$pages){
+                    $next=$now+1;
+                    echo "<a href='?do=$do&p=$next'> > </a>";
+                }
+
+            ?>
+        </div>
 </div>
 <div id="alt"
     style="position: absolute; width: 350px; min-height: 100px; word-break:break-all; text-align:justify;  background-color: rgb(255, 255, 204); top: 50px; left: 400px; z-index: 99; display: none; padding: 5px; border: 3px double rgb(255, 153, 0); background-position: initial initial; background-repeat: initial initial;">
